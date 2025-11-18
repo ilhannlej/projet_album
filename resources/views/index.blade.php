cc7f6efecbe83d6fee89671cd9ce3d912cbf08a1
Lejeune <lejeuneilhann@gmail.com>
2025-11-18 09:02:21 +0100

    remise a 0

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,7 @@
 @extends('template')
 @section("contenu")
 
-<h1>Bienvenue sur le site</h1>
+<h1>Bienvenue sur le </h1>
 <div class="grid">
 
 </div>
--- routes/web.php
+++ routes/web.php
@@ -14,4 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [Main::class, 'index']);
+Route::get('/', function () {
+    return view('welcome');
+});
